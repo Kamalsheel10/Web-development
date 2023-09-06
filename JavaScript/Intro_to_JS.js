8e33471701e41378db7fd99eb58857873b1dee8e
Kamalsheel Mainali <mainaliksm10@gmail.com>
2023-09-06 22:53:57 +0545

    Started to learn JavaSCript.

---
+++ JavaScript/Intro_to_JS.js
@@ -0,0 +1,53 @@
+console.log("Hello Word!");
+console.log("This file is all about the introduction to the javascript.Javascript is considered to be very difficult so let's see!!!");
+let ab = 69;
+console.log(a)
+ab = "Kamalsheel Mainali"
+console.log(ab)
+
+console.log("Understanding var, let and const");
+var a = 34;
+var b = "Raam";
+var c = undefined;
+var d = null;
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+let cd = "Javascript is so boring!!!!";
+console.log(cd);
+
+{
+    let cd = "Javascript is so ineteresting";
+    console.log(cd);
+}
+console.log(cd);
+
+const author = "Kamalsheel Mainali";
+console.log(author);
+
+console.log("Primitive Data Types in JavaScript")
+
+// NN SS BB U 
+
+let a1 = null;
+let b1 = 232;
+let c1 = true;
+let d1 = BigInt("568")
+let e1 = "Kamalsheel Mainali"
+let f1 = Symbol("I am a nice symbol")
+let g1 = undefined;
+
+console.log(a1, b1, c1, d1, e1, f1, g1);
+console.log(typeof (e1));
+
+console.log("Objects in JavaScript");
+const item = {
+    "ram": true,
+    "ravan": false,
+    "Laxman": 2,
+    "SHyam": undefined
+}
+console.log(item["SHyam"]);
---
+++ JavaScript/PracticeSet1.js
@@ -0,0 +1,16 @@
+console.log("Question 1")
+let a="Kamalsheel Mainali"
+let b=9;
+console.log(a+b)
+
+console.log("Question 2")
+console.log(typeof(a+b));
+
+console.log("Question 2")
+const ab={
+    name:"Kamalsheel Mainali",
+    id:211804,
+    isProfessor:false
+}
+// ab="Shyam"
+
